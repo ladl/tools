67add592b0e34194e6171732ca333c6df9ac8e32
ladl <adlylakhdar@gmail.com>
2016-05-28 08:26:07 -0700

    initial set of tools

---
+++ grailsdemo/setenv.sh
@@ -0,0 +1,7 @@
+export ROOT_HOME=/home/helion/dev
+export JAVA_HOME=$ROOT_HOME/jdk1.8.0_73
+#export GRAILS_HOME=$ROOT_HOME/grails-3.1.2
+#export GRAILS_HOME=$ROOT_HOME/grails-2.4.2
+export GRAILS_HOME=$ROOT_HOME/grails-2.5.4
+export STACKATO_HOME=$ROOT_HOME/stackato-3.2.4-linux-glibc2.3-x86_64
+export PATH=$JAVA_HOME/bin:$GRAILS_HOME/bin:$STACKATO_HOME:$PATH
